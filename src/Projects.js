eaf3cf4b4606f565a05c34418f854697bafb75ff
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-15 14:52:55 +0700

    Add content on block project

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,31 @@
 function Projects() {
     return (
         <div>
-
+            <section id="projects" class="clean-block features">
+                <div class="container mx-auto sm:px-4">
+                    <div class="block-heading">
+                        <h2 class="text-teal-500">Projects</h2>
+                    </div>
+                    <div class="flex flex-wrap  justify-center">
+                        <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-star icon" />
+                            <h4>Cryptography</h4>
+                            <p>Development of technical solutions in the field of cryptography<br /></p>
+                        </div>
+                        <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-pencil icon" />
+                            <h4>Transmission security</h4>
+                            <p>Establishment of the defended and channels of communication<br /></p>
+                        </div>
+                        <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-screen-smartphone icon" />
+                            <h4>Blockchain</h4>
+                            <p>Implementation of projects related to blockchain and NFT<br /></p>
+                        </div>
+                        <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-refresh icon" />
+                            <h4>Metaverses</h4>
+                            <p>Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
