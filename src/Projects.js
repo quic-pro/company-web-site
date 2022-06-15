6bd61479df3616841f25f47105f66a5e57dcfba1
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-15 17:49:33 +0700

    add bold font

--- src/Projects.js
+++ src/Projects.js
@@ -8,19 +8,19 @@ function Projects() {
                     </div>
                     <div class="flex flex-wrap  justify-center">
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-star icon" />
-                            <h4>Cryptography</h4>
+                            <h4 class="font-bold">Cryptography</h4>
                             <p>Development of technical solutions in the field of cryptography<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-pencil icon" />
-                            <h4>Transmission security</h4>
+                            <h4 class="font-bold">Transmission security</h4>
                             <p>Establishment of the defended and channels of communication<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-screen-smartphone icon" />
-                            <h4>Blockchain</h4>
+                            <h4 class="font-bold">Blockchain</h4>
                             <p>Implementation of projects related to blockchain and NFT<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-refresh icon" />
-                            <h4>Metaverses</h4>
+                            <h4 class="font-bold">Metaverses</h4>
                             <p>Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
                         </div>
                     </div>
