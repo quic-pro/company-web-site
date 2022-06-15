4ace74b58ab77a500c54d282a2de72d748d10ff4
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-15 18:02:09 +0700

    Chande bold & size

--- src/Projects.js
+++ src/Projects.js
@@ -8,20 +8,20 @@ function Projects() {
                     </div>
                     <div class="flex flex-wrap  justify-center">
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-star icon" />
-                            <h4 class="font-bold">Cryptography</h4>
-                            <p>Development of technical solutions in the field of cryptography<br /></p>
+                            <h4 class="font-bold text-xl py-2">Cryptography</h4>
+                            <p class="text-sm">Development of technical solutions in the field of cryptography<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-pencil icon" />
-                            <h4 class="font-bold">Transmission security</h4>
-                            <p>Establishment of the defended and channels of communication<br /></p>
+                            <h4 class="font-bold text-xl py-2">Transmission security</h4>
+                            <p class="text-sm">Establishment of the defended and channels of communication<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-screen-smartphone icon" />
-                            <h4 class="font-bold">Blockchain</h4>
-                            <p>Implementation of projects related to blockchain and NFT<br /></p>
+                            <h4 class="font-bold text-xl py-2">Blockchain</h4>
+                            <p class="text-sm">Implementation of projects related to blockchain and NFT<br /></p>
                         </div>
                         <div class="md:w-2/5 pr-4 pl-4 feature-box"><i class="icon-refresh icon" />
-                            <h4 class="font-bold">Metaverses</h4>
-                            <p>Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
+                            <h4 class="font-bold text-xl py-2">Metaverses</h4>
+                            <p class="text-sm"> Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
                         </div>
                     </div>
                 </div>
