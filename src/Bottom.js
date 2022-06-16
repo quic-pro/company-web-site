94182bc03863c2d30c45c040a274e716847604fc
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-16 18:05:06 +0700

    add white bottom text

--- src/Bottom.js
+++ src/Bottom.js
@@ -3,27 +3,27 @@ function Bottom() {
         <div>
             <footer class="page-footer dark bg-bottomBlack">
                 <div class="container mx-auto sm:px-4">
-                    <div class="flex flex-wrap ">
-                        <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5>Address</h5>
+                    <div class="flex flex-wrap text-white">
+                        <div class="sm:w-1/4 pr-4 pl-4 ">
+                            <h5 class="font-semibold">Address</h5>
                             <ul>
                                 <li><a>651 N Broad St Suite 206<br />Middletown, DE 19709 US<br /></a></li>
                             </ul>
                         </div>
                         <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5>Phone</h5>
+                            <h5 class="font-semibold">Phone</h5>
                             <ul>
                                 <li><a href="tel:+13028838305">+1 (302) 883 83-05<br /></a></li>
                             </ul>
                         </div>
                         <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5>E-mail</h5>
+                            <h5 class="font-semibold">E-mail</h5>
                             <ul>
                                 <li><a href="mailto:info@quic.pro">info@quic.pro<br /></a></li>
                             </ul>
                         </div>
                         <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5>Legal</h5>
+                            <h5 class="font-semibold">Legal</h5>
                             <ul>
                                 <li><a href="#">Terms of Service</a></li>
                                 <li><a href="#">Terms of Use</a></li>
@@ -33,7 +33,7 @@ function Bottom() {
                     </div>
                 </div>
                 <div class="footer-copyright">
-                    <p>© 2022 QUIC-PRO INC.</p>
+                    <p class="text-white">© 2022 QUIC-PRO INC.</p>
                 </div>
             </footer>
         </div>
