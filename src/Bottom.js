eb700309d7718a5d9a5d4fffd4229cd54c82f9b8
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-16 16:54:15 +0700

    Add code bottom & color

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,7 +1,41 @@
 function Bottom() {
     return (
         <div>
-
+            <footer class="page-footer dark">
+                <div class="container mx-auto sm:px-4">
+                    <div class="flex flex-wrap ">
+                        <div class="sm:w-1/4 pr-4 pl-4">
+                            <h5>Address</h5>
+                            <ul>
+                                <li><a>651 N Broad St Suite 206<br />Middletown, DE 19709 US<br /></a></li>
+                            </ul>
+                        </div>
+                        <div class="sm:w-1/4 pr-4 pl-4">
+                            <h5>Phone</h5>
+                            <ul>
+                                <li><a href="tel:+13028838305">+1 (302) 883 83-05<br /></a></li>
+                            </ul>
+                        </div>
+                        <div class="sm:w-1/4 pr-4 pl-4">
+                            <h5>E-mail</h5>
+                            <ul>
+                                <li><a href="mailto:info@quic.pro">info@quic.pro<br /></a></li>
+                            </ul>
+                        </div>
+                        <div class="sm:w-1/4 pr-4 pl-4">
+                            <h5>Legal</h5>
+                            <ul>
+                                <li><a href="#">Terms of Service</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <p>© 2022 QUIC-PRO INC.</p>
+                </div>
+            </footer>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         'mainBlue':'#09a2ff', // for top bg
         'menuGray':'#000000b3', // for menu text
         'textBlue':'#3b99e0', // eg for "About Us"
+        'bottomBlack':'#2b2f31', // for bottom
       },
       screens: {
         'ph': {'min': '350px', 'max': '800px'},
