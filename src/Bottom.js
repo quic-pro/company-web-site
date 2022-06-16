463d0a45b66df078952d5da7efd1b8f738a48921
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-16 18:56:15 +0700

    padding bottom text

--- src/Bottom.js
+++ src/Bottom.js
@@ -2,28 +2,28 @@ function Bottom() {
     return (
         <div>
             <footer class="page-footer dark bg-bottomBlack">
-                <div class="container mx-auto sm:px-4">
+                <div class="container mx-auto sm:px-4 mx-auto text-center py-5">
                     <div class="flex flex-wrap text-white">
-                        <div class="sm:w-1/4 px-4">
-                            <h5 class="font-semibold">Address</h5>
+                        <div class="sm:w-1/4 px-4 py-2">
+                            <h5 class="font-semibold py-2">Address</h5>
                             <ul>
-                                <li><a>651 N Broad St Suite 206<br />Middletown, DE 19709 US<br /></a></li>
+                                <li><a class="text-sm">651 N Broad St Suite 206<br />Middletown, DE 19709 US<br /></a></li>
                             </ul>
                         </div>
-                        <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5 class="font-semibold">Phone</h5>
+                        <div class="sm:w-1/4 pr-4 pl-4 py-2">
+                            <h5 class="font-semibold py-2">Phone</h5>
                             <ul>
-                                <li><a href="tel:+13028838305">+1 (302) 883 83-05<br /></a></li>
+                                <li><a class="text-sm" href="tel:+13028838305">+1 (302) 883 83-05<br /></a></li>
                             </ul>
                         </div>
-                        <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5 class="font-semibold">E-mail</h5>
+                        <div class="sm:w-1/4 pr-4 pl-4 py-2">
+                            <h5 class="font-semibold py-2">E-mail</h5>
                             <ul>
                                 <li><a href="mailto:info@quic.pro">info@quic.pro<br /></a></li>
                             </ul>
                         </div>
-                        <div class="sm:w-1/4 pr-4 pl-4">
-                            <h5 class="font-semibold">Legal</h5>
+                        <div class="sm:w-1/4 pr-4 pl-4 py-2">
+                            <h5 class="font-semibold py-2">Legal</h5>
                             <ul>
                                 <li><a href="#">Terms of Service</a></li>
                                 <li><a href="#">Terms of Use</a></li>
@@ -32,7 +32,7 @@ function Bottom() {
                         </div>
                     </div>
                 </div>
-                <div class="footer-copyright">
+                <div class="footer-copyright bg-botBottomBlack">
                     <p class="text-white">© 2022 QUIC-PRO INC.</p>
                 </div>
             </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'menuGray':'#000000b3', // for menu text
         'textBlue':'#3b99e0', // eg for "About Us"
         'bottomBlack':'#2b2f31', // for bottom
+        'botBottomBlack':'#212529', // for the bottest
       },
       screens: {
         'ph': {'min': '350px', 'max': '800px'},
