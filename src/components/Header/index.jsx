abfa70601e7e4ec123139c02c52c1a6d211f33b3
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2022-06-17 18:29:51 +0700

    Added styles for header.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,15 +4,15 @@ import React from 'react';
 export default class Header extends React.Component {
     render() {
         return (
-            <header className="flex justify-between">
-                <div className="">
+            <header className="h-14 flex justify-between items-center mx-14">
+                <div className="text-2xl">
                     <a href="/">QUIC-PRO</a>
                 </div>
-                <nav>
-                    <a className="px-4" href="#About">About Us</a>
-                    <a className="px-4" href="#Projects">Projects</a>
-                    <a className="px-4" href="#Team">Team</a>
-                    <a className="px-4" href="#Contact">Contact Us</a>
+                <nav className="text-sm font-bold uppercase text-gray-500">
+                    <a className="px-6" href="#About">About Us</a>
+                    <a className="px-6" href="#Projects">Projects</a>
+                    <a className="px-6" href="#Team">Team</a>
+                    <a className="px-6" href="#Contact">Contact Us</a>
                 </nav>
             </header>
         );
