e2e20bcf5b0475deb32dc183d6cd4d74b24144fe
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-17 14:47:48 +0700

    test2

--- src/TopField.js
+++ src/TopField.js
@@ -9,7 +9,7 @@ function TopField() {
         <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 position: fixed z-50 w-full">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://quic.pro/" class="flex items-center">
-                    <span class="pt-2 pb-1 ml-14  whitespace-no-wrap logo">QUIC-PRO</span>
+                    <span class="pt-2 pb-1 ml-14 text-2xl whitespace-no-wrap logo">QUIC-PRO</span>
                 </a>
                 <button id="buttonMenu" data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
