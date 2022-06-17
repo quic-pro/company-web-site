08c43ee55e641e45445d97e63cd6d06d43222b21
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-17 14:09:24 +0700

    icons & text color in Project

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,7 @@
 import { AiOutlineStar } from 'react-icons/ai';
-import {BsPencil} from 'react-icons/bs';
-import {GiSmartphone} from 'react-icons/gi';
-import {FiRefreshCcw} from 'react-icons/fi';
+import { BsPencil } from 'react-icons/bs';
+import { GiSmartphone } from 'react-icons/gi';
+import { FiRefreshCcw } from 'react-icons/fi';
 
 function Projects() {
     return (
@@ -12,25 +12,33 @@ function Projects() {
                         <h2 class="text-textBlue text-3xl font-medium text-center py-9">Projects </h2>
                     </div>
                     <div class="flex flex-wrap  justify-center">
-                        <div class="md:w-2/5 px-4 pt-5 feature-box">
-                            <AiOutlineStar class="fill-textBlue box-border h-10 w-10 float-left p-1"/>
-                            <h4 class="font-bold text-xl py-2">Cryptography</h4>
-                            <p class="text-sm">Development of technical solutions in the field of cryptography<br /></p>
+                        <div class="flex flex-row md:w-2/5 px-4 pt-5 feature-box">
+                            <AiOutlineStar class="fill-textBlue box-border h-20 w-20 pr-4" />
+                            <div class="grow">
+                                <h4 class="font-bold text-xl py-2">Cryptography</h4>
+                                <p class="text-sm text-botBottomBlack">Development of technical solutions in the field of cryptography<br /></p>
+                            </div>
                         </div>
-                        <div class="md:w-2/5 px-4 pt-5 feature-box">
-                            <BsPencil class="fill-textBlue box-border h-10 w-10 float-left p-1"/>
-                            <h4 class="font-bold text-xl py-2">Transmission security</h4>
-                            <p class="text-sm">Establishment of the defended and channels of communication<br /></p>
+                        <div class="flex flex-row md:w-2/5 px-4 pt-5 feature-box">
+                            <BsPencil class="fill-textBlue box-border h-20 w-20 pr-5" />
+                            <div class="grow">
+                                <h4 class="font-bold text-xl py-2">Transmission security</h4>
+                                <p class="text-sm text-botBottomBlack">Establishment of the defended and channels of communication<br /></p>
+                            </div>
                         </div>
-                        <div class="md:w-2/5 px-4 pt-5 feature-box">
-                            <GiSmartphone class="fill-textBlue box-border h-10 w-10 float-left p-1"/>
-                            <h4 class="font-bold text-xl py-2">Blockchain</h4>
-                            <p class="text-sm">Implementation of projects related to blockchain and NFT<br /></p>
+                        <div class="flex flex-row md:w-2/5 px-4 pt-5 feature-box">
+                            <GiSmartphone class="fill-textBlue box-border h-20 w-20 pr-5" />
+                            <div class="grow">
+                                <h4 class="font-bold text-xl py-2">Blockchain</h4>
+                                <p class="text-sm text-botBottomBlack">Implementation of projects related to blockchain and NFT<br /></p>
+                            </div>
                         </div>
-                        <div class="md:w-2/5 px-4 pt-5 feature-box">
-                            <FiRefreshCcw class="stroke-textBlue box-border h-10 w-10 float-left p-1"/>
-                            <h4 class="font-bold text-xl py-2s">Metaverses</h4>
-                            <p class="text-sm"> Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
+                        <div class="flex flex-row md:w-2/5 px-4 pt-5 feature-box">
+                            <FiRefreshCcw class="stroke-textBlue box-border h-20 w-20 pr-4" />
+                            <div class="grow pl-3">
+                                <h4 class="font-bold text-xl py-2">Metaverses</h4>
+                                <p class="text-sm text-botBottomBlack"> Combining real world and metaverses through digital technological introduction&nbsp;&nbsp;<br /></p>
+                            </div>
                         </div>
                     </div>
                 </div>
