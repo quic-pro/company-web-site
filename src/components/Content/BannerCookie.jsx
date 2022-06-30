3c560b0017e908b4b2235844a75eab9c5cbd0c09
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-30 15:12:23 +0700

    Change color buttons on banner

--- src/components/Content/BannerCookie.jsx
+++ src/components/Content/BannerCookie.jsx
@@ -20,12 +20,12 @@ export default class BannerCookie extends React.Component {
             </div>
             <div className="flex flex-row">
               <div className="cursor-pointer px-6 pt-2 pb-4 md:px-0 md:pt-0 md:pb-0" data-behavior="decline-cookie-consent">
-                <button className="md:mb-0 ml-0 md:ml-4 px-3 py-1 border-companyBlue-600 rounded-lg bg-white text-companyBlue-600 text-xl">
+                <button className="md:mb-0 ml-0 md:ml-4 px-3 py-1 border-companyBlue-600 rounded-lg bg-companyBlue-100 text-companyBlue-600 hover:bg-companyBlue-150 text-xl">
                   <a href="https://www.google.com/"> Decline </a>
                 </button>
               </div>
               <div className="cursor-pointer px-6 pt-2 pb-4 md:px-0 md:pt-0 md:pb-0" data-behavior="accept-cookie-consent">
-                <button className="md:mb-0 ml-0 md:ml-4 px-3 py-1 border-companyBlue-600 rounded-lg bg-white text-companyBlue-600 text-xl"
+                <button className="md:mb-0 ml-0 md:ml-4 px-3 py-1 border-companyBlue-600 rounded-lg bg-companyBlue-100 text-companyBlue-600 hover:bg-companyBlue-150 text-xl"
                   onClick={() => this.setState({isAccept:true})}
                 >
                   Accept
