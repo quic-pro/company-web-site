5a5031babdc3f2746773b419e676bbedf13f23f7
RRRRRAMzik <75689935+RRRRRAMzik@users.noreply.github.com>
2023-01-25 01:01:43 +0700

    refactor: rewrite reportWebVitals component to typescript

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,16 @@
+import {ReportHandler} from 'web-vitals';
+
+export function reportWebVitals(onPerfEntry?: ReportHandler) {
+    if (onPerfEntry) {
+        import('web-vitals')
+            .then(({getINP, getCLS, getFCP, getLCP, getFID, getTTFB}) => {
+                getINP(onPerfEntry);
+                getCLS(onPerfEntry);
+                getFCP(onPerfEntry);
+                getFID(onPerfEntry);
+                getLCP(onPerfEntry);
+                getTTFB(onPerfEntry);
+            })
+            .catch(console.error);
+    }
+}
